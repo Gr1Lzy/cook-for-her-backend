98dfc8995ff3ee3b7e75f6468fa3296df9b5667a
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-08-09 14:04:28 +0300

    [dev] FEATURE | Added basic Spring Application

---
+++ src/main/java/com/github/cookforher/CookForHerApplication.java
@@ -0,0 +1,12 @@
+package com.github.cookforher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookForHerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CookForHerApplication.class, args);
+  }
+}
