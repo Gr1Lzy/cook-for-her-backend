d726f3fe1fc7c63b4c770f41255c0ed0f862c6c5
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-08-10 21:57:23 +0300

    [dev] FEATURE | Added database H2 for dev with basic JPA

--- src/main/java/com/github/cookforher/CookForHerApplication.java
+++ src/main/java/com/github/cookforher/CookForHerApplication.java
@@ -1,12 +1,22 @@
 package com.github.cookforher;
 
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class CookForHerApplication {
+public class CookForHerApplication implements ApplicationRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(CookForHerApplication.class, args);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
   }
 }
