9580a98a34d4e36b6380aaead11ff8d06f94d715
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-08-11 21:27:34 +0300

    [dev] FEATURE | Added Entity for tables Role and User

---
+++ src/main/java/com/github/cookforher/entity/Role.java
@@ -0,0 +1,25 @@
+package com.github.cookforher.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+  ROLE_ADMIN(1),
+  ROLE_USER(2);
+
+  private final Integer id;
+
+  Role(Integer id) {
+    this.id = id;
+  }
+
+  public static Role fromId(Integer id) {
+    for (Role role : Role.values()) {
+      if (role.getId().equals(id)) {
+        return role;
+      }
+    }
+
+    throw new IllegalArgumentException("Invalid role id: " + id);
+  }
+}
