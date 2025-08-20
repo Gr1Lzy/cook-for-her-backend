cc3e919cc8d49c1bf4ca1fc65e2f2c83dd3c410f
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-08-20 19:43:43 +0300

    [dev] FEATURE | Updated logic for GlobalExceptionHandler in JWT

--- src/main/java/com/github/cookforher/util/jwt/JwtTokenUtil.java
+++ src/main/java/com/github/cookforher/util/jwt/JwtTokenUtil.java
@@ -43,11 +43,15 @@ public class JwtTokenUtil {
   }
 
   public boolean isAccessTokenValid(String token) {
-    return isValidToken(token, ACCESS_TOKEN);
+    try {
+      return isValidToken(token, ACCESS_TOKEN);
+    } catch (Exception e) {
+      return false;
+    }
   }
 
   public boolean isRefreshTokenValid(String token) {
-    return isValidToken(token, REFRESH_TOKEN);
+      return isValidToken(token, REFRESH_TOKEN);
   }
 
   public String generateAccessToken(UserDetails userDetails) {
--- src/main/java/com/github/cookforher/util/user/UserUtil.java
+++ src/main/java/com/github/cookforher/util/user/UserUtil.java
@@ -1,12 +1,23 @@
 package com.github.cookforher.util.user;
 
+import com.github.cookforher.exception.custom.AuthenticationException;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @UtilityClass
 public class UserUtil {
+  private static final String ANONYMOUS_USER = "anonymousUser";
 
   public static Long getCurrentUserId() {
+    if (isAnonymousUser()) {
+      throw new AuthenticationException("Anonymous user is not allowed to access this resource");
+    }
+
     return (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+  }
+
+  private boolean isAnonymousUser() {
+    String user = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+    return user.equals(ANONYMOUS_USER);
   }
 }
